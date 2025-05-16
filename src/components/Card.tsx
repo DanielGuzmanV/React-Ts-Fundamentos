c478831abfa8f821b1796d000df89917de50afa0
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-16 16:51:47 -0400

    se agrego un fragment al archivo -Card-

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react/jsx-runtime";
+
 function Card() {
   return (
     <div className="card"
@@ -7,22 +9,30 @@ function Card() {
       }}
     >
       <div className="card-body" style={{backgroundColor: 'aquamarine'}}>
-
-        <h5 className="card-title" style={{color: 'red'}}>Titulo de la targeta</h5>
-
-        <p className="card-text">
-          Some quick example text to build on the card title and make
-          up the bulk of the card's content.
-        </p>
-
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-
+        < CardBody/>
       </div>
     </div>
   );
 
+}
+
+function CardBody() {
+  // tambien podemos usar <></> en ves de <Fragment></Fragment> asi no importamos
+  // import { Fragment } from "react/jsx-runtime";
+  return (
+    <Fragment>
+      <h5 className="card-title" style={{color: 'red'}}>Titulo de la targeta</h5>
+
+      <p className="card-text">
+        Some quick example text to build on the card title and make
+        up the bulk of the card's content.
+      </p>
+
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </Fragment>
+  )
 }
 
 export default Card;

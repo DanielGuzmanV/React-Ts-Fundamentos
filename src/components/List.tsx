aeb612b9022e4619f10ca43edf386a33b0a2dee1
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-17 11:01:51 -0400

    Se agrego una gestion de estado a -List-

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 type Props = {
   dataProps: string[];
 }
 
 function List({dataProps}: Props) {
+  const [index, setIndex] = useState(0);
 
-  const handleClick = (event: string) => {
+  const handleClick = (event: string, indice: number) => {
+    setIndex(indice);
     console.log('Se preciono sobre:', event);
-    alert(event);
   }
 
   return (
     <ul className="list-group">
-      {dataProps.map( (elementos) => (
-        <li onClick={ () => handleClick(elementos)} key={elementos} className="list-group-item">
+      {dataProps.map( (elementos, idx) => (
+        <li onClick={ 
+            () => handleClick(elementos, idx)
+            } 
+          key={elementos} 
+          className={`list-group-item ${index == idx ? "active" : ""}`}
+        >
           {elementos}
         </li>
       ))}

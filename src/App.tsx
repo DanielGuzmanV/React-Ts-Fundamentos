93479b53fd97261a13fcc460edb91d3d7127f442
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-28 10:29:40 -0400

    se movio Apptsx a CardListtsx en components

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,12 @@
-import Card, {CardBody} from "./components/Card"
-import List from "./components/List";
-import Button from "./components/Button";
-import { useState } from "react";
+import CardList from "./components/CardList";
 
 function App() {
-
-  // Realizamos el "useState" para el button
-  const [isLoading, setIsLoading] = useState(false);
-  const changeHandleClick = () => setIsLoading(!isLoading);
-
-
-  const listString = ['JavaScript', 'TypeScript', 'React', 'NodeJs'];
-  // Agregar elementos a "listString2" si es necesario
-  const listString2: string[] = [];
-  
-  const handleSelect1 = (selectElement: string) => {
-    console.log('Se imprimio en consola:', selectElement)
-  }
-  const handleSelect2 = (selectElement: string) => {
-    console.log('Mostrando:', selectElement)
-  }
-
-  const content = listString2.length !== 0 
-    ? (<List dataProps={listString2} onSelect={handleSelect2}/>)
-    : 'Primera lista sin elementos:'
   
   
   return (
-    <Card>
-      <CardBody title="Hola React" text="Esto es un parrafo usando React"/>
-      {content}
-
-      {/*  Podemos usar el "Short Circuit Operator" */}
-      {listString.length !== 0 && (
-        <List dataProps={listString} onSelect={handleSelect1}/>
-      )}
-
-
-      <Button isLoadingButton = {isLoading} onClickButton={changeHandleClick}>
-        Hola react
-      </Button>
-
-    </Card>
+    <div>
+      <CardList/>
+    </div>
   )
 
   // return <CardBody title="Hola React" text="Esto es un texto usando React"/>

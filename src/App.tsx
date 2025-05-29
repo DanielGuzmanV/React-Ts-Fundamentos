bc9fe04d793ad25afee03935530a883581c56ad2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-29 17:24:01 -0400

    Nuevo componente creado

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import Card, {CardTitle} from "./components/Card"
-import List from "./components/List";
-import Button from "./components/Button";
-import { useState } from "react";
+import ButtonCustomView from "./components/buttonCustom/ButtonCustomView";
 
 function App() {
-  const listString = ['JavaScript', 'TypeScript', 'React', 'NodeJs'];
-  const [data, setData] = useState(listString);
-  
-  // Vemos el elemento seleccionado en consola:
-  const handleSelect = (selectElement: string) => {
-    console.log('Se imprimio en consola:', selectElement)
-  }
-
-  // Agregar elementos:
-  const addHandleClick = () => {
-    return setData([...data, 'Values React'])
-  }
-  // Eliminar elementos:
-  const delHandleClick = () => {
-    return setData(data.slice(0, -1));
-  }
   
   return (
-    <Card>
-      <CardTitle title="Lista dinamica"/>
-
-      <Button onClickButton={addHandleClick} isLoadingBtn = {false}>
-        Agregar
-      </Button>
-      <Button onClickButton={delHandleClick} isLoadingBtn = {true}>
-        Eliminar
-      </Button>
-
-      <List dataProps={data} onSelect={handleSelect}/>
-
-    </Card>
+  
+    <ButtonCustomView/>
+  
   )
 
 }
---
+++ src/components/buttonCustom/ButtonCustomView.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Card, { CardTitle } from "../Card"
+import Button from "../Button";
+import List from "../List";
+
+
+function ButtonCustomView() {
+  const arrStrinValues = ['JavaScript', 'TypeScript', 'React', 'NodeJs'];
+
+  const [data, setData] = useState(arrStrinValues);
+
+  const handleSelect = function(selectElem: string) {
+    alert(`Elemento: ${selectElem}`);
+  }
+
+  // Agregar elementos:
+  const addHandleClick = () => {
+    return setData([...data, 'Value React']);
+  }
+
+  // Eliminar elementos:
+  const deleteHandleClick = () => {
+    return setData(data.slice(0, -1));
+  }
+
+
+  return (
+  <Card>
+    <CardTitle title="Botones dinamicos"/>
+
+    <Button onClickButton={addHandleClick} isLoadingBtn = {false}>
+      Agregar
+    </Button>
+
+    <Button onClickButton={deleteHandleClick} isLoadingBtn = {true}>
+      Eliminar
+    </Button>
+
+    <List dataProps={data} onSelect={handleSelect}/>
+
+  </Card>
+  )
+}
+
+export default ButtonCustomView

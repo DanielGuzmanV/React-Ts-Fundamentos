2d4d5df7009d3827937a85dcaa3fa9e1528031d6
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-21 11:51:33 -0400

    se creo el main principal

---
+++ src/pages/pages_AmazingCode/MainAmazaingCode.tsx
@@ -0,0 +1,15 @@
+import ButtonsAmazaingcode from "../../components/ButtonsAmazaingcode"
+
+function MainAmazingCode() {
+  return (
+    <div>
+      <header>
+        <h1 className="title">Fundamentos "AMazingCode"</h1>
+      </header>
+
+      <ButtonsAmazaingcode/>
+    </div>
+  )
+}
+
+export default MainAmazingCode
